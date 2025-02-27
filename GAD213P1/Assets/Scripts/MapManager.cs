f91060ffe442a07d70780cccfbba8936a8a99c8f
Aisha Alotbi <aisha.alotbi@student.saedubai.com>
2025-02-27 18:53:23 +0400

    Added Overlay Tiles and cursor movement

---
+++ GAD213P1/Assets/Scripts/MapManager.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+
+{
+    private static MapManager _instance;
+    public static MapManager Instance { get { return _instance; } }
+
+    public OverlayTile overlayTilePrefab;
+    public GameObject overlayContainer;
+
+    public Dictionary<Vector2Int, OverlayTile> map;
+
+
+
+    private void Awake()
+    {
+        if (_instance !=null && _instance != this)
+        {
+            Destroy(this.gameObject);
+
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        var tileMap = gameObject.GetComponentInChildren<Tilemap>();
+        map = new Dictionary<Vector2Int, OverlayTile>();
+
+        BoundsInt bounds = tileMap.cellBounds;
+
+        // Loop through all tiles 
+        
+        
+            for (int y = bounds.min.y; y < bounds.max.y; y++)
+            {
+                for (int x = bounds.min.x; x < bounds.max.x; x++)
+                {
+                    Vector3Int tileLocation = new Vector3Int(x, y, (int)tileMap.transform.position.y);
+                    Vector3 place = tileMap.CellToWorld(tileLocation);
+                    var tileKey = new Vector2Int(x, y);
+
+                    if (tileMap.HasTile(tileLocation) && !map.ContainsKey(tileKey))
+                    // instantiates cursor 
+                    {
+                        var overlayTile = Instantiate(overlayTilePrefab, overlayContainer.transform);
+                        var cellWorldPosition = tileMap.GetCellCenterWorld(tileLocation);
+
+                        overlayTile.transform.position = new Vector3(cellWorldPosition.x, cellWorldPosition.y, cellWorldPosition.z + 1);
+                        overlayTile.GetComponent<SpriteRenderer>().sortingOrder = tileMap.GetComponent<TilemapRenderer>().sortingOrder + 1;
+                        map.Add(tileKey, overlayTile);
+                    }
+
+                }
+            }
+        
+    }
+
+}
+
+
